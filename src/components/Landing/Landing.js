0c84c7a383ef978e8fea1c389ef9870a0dddc2f1
Andrew Johnson <andrew.johnson2073@gmail.com>
2019-01-03 15:50:24 -0600

    Corrected a typo and emptied the server

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -4,7 +4,7 @@ import './landing.css';
 const Landing = () => {
   return (
     <div className="landing">
-      <h1>Welcome to Simple Post!</h1>
+      <h1>Welcome to Simple Blog!</h1>
       <p>
         To get started creating a post, click on the CREATE link at the top of
         the page. To see your posts, select the POSTS link.
